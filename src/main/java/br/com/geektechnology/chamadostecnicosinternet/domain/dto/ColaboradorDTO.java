820e7ec1502fe61baa58624c17e6991dff063221
carnegiejunior <carnegiejunior@gmail.com>
2020-04-07 14:07:33 -0300

    Alteração no arquivo DTO

--- src/main/java/br/com/geektechnology/chamadostecnicosinternet/domain/dto/ColaboradorDTO.java
+++ src/main/java/br/com/geektechnology/chamadostecnicosinternet/domain/dto/ColaboradorDTO.java
@@ -2,7 +2,9 @@ package br.com.geektechnology.chamadostecnicosinternet.domain.dto;
 
 import br.com.geektechnology.chamadostecnicosinternet.domain.model.colaborador.FuncaoClass;
 import br.com.geektechnology.chamadostecnicosinternet.domain.model.pessoa.Pessoa;
+import lombok.Data;
 
+@Data
 public class ColaboradorDTO {
 	
 	private Long id;
